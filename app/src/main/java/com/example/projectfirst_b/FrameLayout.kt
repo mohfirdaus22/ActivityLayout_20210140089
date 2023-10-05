515a98eca67e45bc6636793370a245c760391610
mohfirdaus22 <thepashter6@gmail.com>
2023-10-05 20:50:01 +0700

    add Constrain Layout dan Frame Layout

---
+++ app/src/main/java/com/example/projectfirst_b/FrameLayout.kt
@@ -0,0 +1,11 @@
+package com.example.projectfirst_b
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FrameLayout : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.framelayout_act)
+    }
+}
