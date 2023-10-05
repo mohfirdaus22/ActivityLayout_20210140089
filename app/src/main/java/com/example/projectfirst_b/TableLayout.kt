787ee64b199136790abdec32761dfb86a4efe2fd
mohfirdaus22 <thepashter6@gmail.com>
2023-10-05 22:38:07 +0700

    Add Linear_layout dan Table Layout

---
+++ app/src/main/java/com/example/projectfirst_b/TableLayout.kt
@@ -0,0 +1,11 @@
+package com.example.projectfirst_b
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class TableLayout : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.table_layout)
+    }
+}
